228c37ba3a0dc484e1be5a04ee41b5e1727f5bcd
山本 <@TSD-YAMAMOTO>
2019-07-09 10:57:17 +0900

    logoutの作成

---
+++ src/com/abc/asms/servlets/LogoutServlet.java
@@ -0,0 +1,8 @@
+package com.abc.asms.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/C0010.html")
+public class LogoutServlet extends HttpServlet {
+	
+}
