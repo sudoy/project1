97467166f7738deedc66edb68d6ab44294235eb4
山本 <@TSD-YAMAMOTO>
2019-07-09 10:29:55 +0900

    login

---
+++ src/com/abc/asms/Servlets/LoginServlet.java
@@ -0,0 +1,7 @@
+package com.abc.asms.Servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class LoginServlet extends HttpServlet {
+
+}
---
+++ src/com/abc/asms/services/LoginService.java
@@ -0,0 +1,7 @@
+package com.abc.asms.services;
+
+import javax.servlet.http.HttpServlet;
+
+public class LoginService extends HttpServlet {
+
+}
