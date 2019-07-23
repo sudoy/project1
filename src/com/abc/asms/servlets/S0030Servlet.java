f45d3139570a5c095de998ef95baddeba2885bc4
山本 <@TSD-YAMAMOTO>
2019-07-23 13:00:40 +0900

    s

--- src/com/abc/asms/servlets/S0030Servlet.java
+++ src/com/abc/asms/servlets/S0030Servlet.java
@@ -24,7 +24,7 @@ public class S0030Servlet extends HttpServlet {
 
 		//権限チェック→アカウント登録権限がない場合はダッシュボードへ
 		HttpSession session = req.getSession();
-		AccountForm account = (AccountForm) session.getAttribute("account");
+		AccountForm account = (AccountForm) session.getAttribute("accounts");
 		int authority = account.getAuthority();
 		if(authority != 10 && authority != 11) {
 			List<String> error = new ArrayList<>();
@@ -60,7 +60,7 @@ public class S0030Servlet extends HttpServlet {
 
 		//権限チェック→アカウント登録権限がない場合はダッシュボードへ
 		HttpSession session = req.getSession();
-		AccountForm account = (AccountForm) session.getAttribute("account");
+		AccountForm account = (AccountForm) session.getAttribute("accounts");
 		int authority = account.getAuthority();
 		if(authority != 10 && authority != 11) {
 			List<String> error = new ArrayList<>();
